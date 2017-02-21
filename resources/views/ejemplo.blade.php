c848136cf423c54111b61924006c1f011354967c
Luis Santillan <santillan.itc@gmail.com>
2017-02-21 11:01:29 -0700

    Proyecto en blanco

---
+++ resources/views/ejemplo.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Vista de ejemplo</title>
+</head>
+<body>
+	<h1>Vista de Ejemplo</h1>
+	<hr>
+	<p>Este es un ejemplo e una vista en Laravel</p>
+</body>
+</html>
